5b5741ecc75525380213b4a8804295f5c2fb839f
Justin <jmp86@yahoo.com>
2021-08-24 17:00:21 -1000

    commit tree b5aaada82a46cfa1de2793297d1311b3cb441e83

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        new_arr = []
+        array.map do |word|
+           if @word.chars.sort == word.chars.sort
+            new_arr << word
+           end
+        end
+        new_arr
+    end
+end
